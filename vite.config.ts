554bc0766ba50dd25fc4c2b4e6d342b66205206a
sanuxmudulu <sanuxmudulu@gmail.com>
2026-01-12 20:25:01 +0545

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["@tanstack/react-query"], // 👈 add this line
+    },
+  },
+});
